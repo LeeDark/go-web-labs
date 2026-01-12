6ec75280b35053d62364c38c776775182563c614
Sergey Povarnin <sergey.povarnin@gmail.com>
2026-01-12 09:29:10 +0200

    2: letsgo: Clean comments.

--- books/lets-go/snippetbox/cmd/web/main.go
+++ books/lets-go/snippetbox/cmd/web/main.go
@@ -14,33 +14,15 @@ type config struct {
 }
 
 func main() {
-	// Define a new command-line flag with the name 'addr', a default value of ":4000"
-	// and some short help text explaining what the flag controls. The value of the
-	// flag will be stored in the addr variable at runtime.
-	//addr := flag.String("addr", ":4000", "HTTP network address")
 	var cfg config
 	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
 	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
-
-	// Importantly, we use the flag.Parse() function to parse the command-line flag.
-	// This reads in the command-line flag value and assigns it to the addr
-	// variable. You need to call this *before* you use the addr variable
-	// otherwise it will always contain the default value of ":4000". If any errors are
-	// encountered during parsing the application will be terminated.
 	flag.Parse()
 
-	// Use the http.NewServeMux() function to initialize a new servemux, then
-	// register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
 
-	// Create a file server which serves files out of the "./ui/static" directory.
-	// Note that the path given to the http.Dir function is relative to the project
-	// directory root.
 	fileServer := http.FileServer(neutered.NewFileSystem(http.Dir(cfg.staticDir)))
 
-	// Use the mux.Handle() function to register the file server as the handler for
-	// all URL paths that start with "/static/". For matching paths, we strip the
-	// "/static" prefix before the request reaches the file server.
 	mux.Handle("/static", http.NotFoundHandler())
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
@@ -49,15 +31,8 @@ func main() {
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	// Print a log message to say that the server is starting
 	log.Printf("starting server on %s", cfg.addr)
 
-	// Use the http.ListenAndServe() function to start a new web server.
-	// We pass in two parameters: the TCP network address to listen on (in this case ":4000")
-	// and the servemux we just created.
-	// If http.ListenAndServe() returns an error we use the log.Fatal() fucntion
-	// to log the error message and exit.
-	// Note that any error returned by http.ListenAndServe() is always non-nil.
 	err := http.ListenAndServe(cfg.addr, mux)
 	log.Fatal(err)
 }
