ef5d9e56267ca6e83a57a876075d1c5dcfc5bc01
Sergey Povarnin <sergey.povarnin@gmail.com>
2026-01-22 19:35:10 +0200

    2: letsgo: 5. Template actions and functions.

--- books/lets-go/snippetbox/cmd/web/templates.go
+++ books/lets-go/snippetbox/cmd/web/templates.go
@@ -3,5 +3,6 @@ package main
 import "github.com/LeeDark/go-web-labs/books/lets-go/snippetbox/internal/models"
 
 type templateData struct {
-	Snippet models.Snippet
+	Snippet  models.Snippet
+	Snippets []models.Snippet
 }
