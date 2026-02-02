dd316c9b5721cfb071242b22d21157b1057a60fd
Sergey Povarnin <sergey.povarnin@gmail.com>
2026-02-03 00:42:14 +0200

    2: letsgo: 5. Dynamic HTML templates.

--- books/lets-go/snippetbox/cmd/web/handlers.go
+++ books/lets-go/snippetbox/cmd/web/handlers.go
@@ -20,9 +20,10 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	app.render(w, r, http.StatusOK, "home.tmpl", templateData{
-		Snippets: snippets,
-	})
+	data := app.newTemplateData(r)
+	data.Snippets = snippets
+
+	app.render(w, r, http.StatusOK, "home.tmpl", data)
 }
 
 // Add a snippetView handler function.
@@ -44,9 +45,10 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	app.render(w, r, http.StatusOK, "view.tmpl", templateData{
-		Snippet: snippet,
-	})
+	data := app.newTemplateData(r)
+	data.Snippet = snippet
+
+	app.render(w, r, http.StatusOK, "view.tmpl", data)
 }
 
 // Add a snippetCreate handler function.
