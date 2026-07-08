259f3ae7805dc33e44fb9aa87c2f7d98cb52d819
Sergey Povarnin <sergey.povarnin@gmail.com>
2026-07-08 20:26:22 +0300

    Restricting Inputs. Custom JSON Decoding.

--- books/lets-go-further/greenlight/cmd/api/helpers.go
+++ books/lets-go-further/greenlight/cmd/api/helpers.go
@@ -47,7 +47,11 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data envelo
 	return nil
 }
 func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any) error {
-	err := json.NewDecoder(r.Body).Decode(dst)
+	r.Body = http.MaxBytesReader(w, r.Body, 1_048_576)
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	err := dec.Decode(dst)
 	if err != nil {
 		var syntaxError *json.SyntaxError
 		var unmarshalTypeError *json.UnmarshalTypeError
@@ -75,6 +79,11 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst any
 		default:
 			return err
 		}
+	}
+
+	err = dec.Decode(&struct{}{})
+	if !errors.Is(err, io.EOF) {
+		return errors.New("body must only contain a single JSON value")
 	}
 
 	return nil
